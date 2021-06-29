f90c6fad2c07079094ebd5f57d800d86d1746723
Mohit Kumar <mkmohities7@gmail.com>
2021-06-30 00:14:37 +0100

    Created method to calculate price

--- src/main/java/uk/co/orderprocessing/model/sku/SKU.java
+++ src/main/java/uk/co/orderprocessing/model/sku/SKU.java
@@ -1,12 +1,7 @@
 package uk.co.orderprocessing.model.sku;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 public class SKU {
     private SKUType itemType;
-
-    @JsonIgnore
-    private int value;
 
     public SKUType getItemType() {
         return itemType;
@@ -16,31 +11,12 @@ public class SKU {
         this.itemType = itemType;
     }
 
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public SKU(SKUType itemType, int value) {
-        this.itemType = itemType;
-        this.value = value;
-    }
-
     public SKU(String itemType) {
         this.itemType = SKUType.valueOf(itemType);
+
     }
 
     public SKU() {
     }
 
-    @Override
-    public String toString() {
-        return "SKU{" +
-                "itemType=" + itemType +
-                ", value=" + value +
-                '}';
-    }
 }
--- src/main/java/uk/co/orderprocessing/service/OrderService.java
+++ src/main/java/uk/co/orderprocessing/service/OrderService.java
@@ -1,2 +1,37 @@
-package uk.co.orderprocessing.service;public class OrderService {
+package uk.co.orderprocessing.service;
+
+import org.springframework.stereotype.Service;
+import uk.co.orderprocessing.model.Order;
+import uk.co.orderprocessing.model.sku.SKUType;
+import uk.co.orderprocessing.model.sku.SKUWithCount;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+
+import static uk.co.orderprocessing.model.sku.SKUType.*;
+
+@Service
+public class OrderService {
+
+    private Map<SKUType, Long> pricesMap;
+
+    private Long calculatePriceWithoutPromo(Order order) {
+        Long price = 0L;
+        if(order != null){
+            for (SKUWithCount item: order.getItems()){
+                price = price + ( item.getCount() *  pricesMap.get(item.getSku().getItemType()));
+            }
+        }
+        return price;
+    }
+
+    // This service will fetch data fro third party in production
+    @PostConstruct
+    private void populatePriceMap(){
+        pricesMap.put(A, 50L);
+        pricesMap.put(B, 30L);
+        pricesMap.put(C, 20L);
+        pricesMap.put(D, 15L);
+    }
+
 }
